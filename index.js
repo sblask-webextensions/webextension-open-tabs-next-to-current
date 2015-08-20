46f729df99ca7183bf22ced8b9b1e3940bdba828
Sebastian Blask <blask@gmx.de>
2015-08-20 16:02:29 +0200

    Some renaming

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const core = require("sdk/view/core");
-const helpers = require("./helpers.js");
 const hotkeys = require("sdk/hotkeys");
 const tabs = require("sdk/tabs");
 const windows = require("sdk/windows").browserWindows;
+
+const helpers = require("./lib/helpers");
 
 hotkeys.Hotkey({
     combo: "accel-alt-t",
@@ -11,12 +12,12 @@ hotkeys.Hotkey({
 
 tabs.on("open", helpers.maybeMoveTab);
 
-function registerWindowClickListener(window) {
+function registerListeners(window) {
     let lowLevelWindow = core.viewFor(window);
-    lowLevelWindow.addEventListener("click", helpers.maybeDisableTabMoving);
+    lowLevelWindow.addEventListener("click", helpers.doNotMoveTabIfNewTabButtonClick);
 }
 
 for (let window of windows) {
-    registerWindowClickListener(window);
+    registerListeners(window);
 }
-windows.on('open', registerWindowClickListener);
+windows.on('open', registerListeners);
