e9aa16c889b55873710022350b566965c5aec6b3
Sebastian Blask <blask@gmx.de>
2015-10-08 20:32:32 +0200

    Handle session restoring

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const tabs = require("sdk/tabs");
 const windows = require("sdk/windows").browserWindows;
 
 const helpers = require("./lib/helpers");
+const state = require("./lib/state");
 
 hotkeys.Hotkey({
     combo: "accel-alt-t",
@@ -15,6 +16,9 @@ tabs.on("open", helpers.maybeMoveTab);
 function registerListeners(window) {
     let lowLevelWindow = core.viewFor(window);
     lowLevelWindow.addEventListener("click", helpers.maybeDisableIfNewTabButtonClick);
+
+    lowLevelWindow.addEventListener("SSWindowStateBusy", function(){ state.disableUntilEnabled(); });
+    lowLevelWindow.addEventListener("SSWindowStateReady", function(){ state.enable(); });
 }
 
 for (let window of windows) {
