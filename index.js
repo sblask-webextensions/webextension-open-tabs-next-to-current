a9d8cbdfddd7d5c13456c12e06825ab1ca4ce916
Sebastian Blask <blask@gmx.de>
2015-10-14 17:15:57 +0200

    Fix jscs errors

--- index.js
+++ index.js
@@ -17,8 +17,8 @@ function registerListeners(window) {
     let lowLevelWindow = core.viewFor(window);
     lowLevelWindow.addEventListener("click", helpers.maybeDisableIfNewTabButtonClick, true);
 
-    lowLevelWindow.addEventListener("SSWindowStateBusy", function(){ state.disableUntilEnabled(); });
-    lowLevelWindow.addEventListener("SSWindowStateReady", function(){ state.enable(); });
+    lowLevelWindow.addEventListener("SSWindowStateBusy", function() { state.disableUntilEnabled(); });
+    lowLevelWindow.addEventListener("SSWindowStateReady", function() { state.enable(); });
 }
 
 for (let window of windows) {
