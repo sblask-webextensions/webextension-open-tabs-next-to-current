b95323f0c0773ebfcc42ca11d5dfb85a581b3e84
Sebastian Blask <blask@gmx.de>
2026-07-28 14:20:18 +1200

    Update prepare-chrome.sh

--- bin/prepare-chrome.sh
+++ bin/prepare-chrome.sh
@@ -15,7 +15,7 @@ VERSION=$1
 
 cp "${ROOT_DIRECTORY}"/LICENSE "${DIST_DIRECTORY}"
 
-jq --indent 4 ". | .version |= \"${VERSION}\" | del(.browser_specific_settings) | del(.browser_action.default_icon) | del(.background.scripts) | .permissions -= [\"sessions\"]" "${ROOT_DIRECTORY}"/manifest.json > "${DIST_DIRECTORY}"/manifest.json
+jq --indent 4 ". | .version |= \"${VERSION}\" | del(.browser_specific_settings) | del(.action.default_icon) | del(.background.scripts) | .permissions -= [\"sessions\"]" "${ROOT_DIRECTORY}"/manifest.json > "${DIST_DIRECTORY}"/manifest.json
 
 # copy files
 cp "${ROOT_DIRECTORY}"/icons/*.{png,svg} "${DIST_DIRECTORY}"
@@ -31,6 +31,9 @@ if [ -d "${ROOT_DIRECTORY}"/options ]; then
 fi
 cp "${ROOT_DIRECTORY}"/*.js "${DIST_DIRECTORY}"/
 rm "${DIST_DIRECTORY}"/eslint.config.js
+
+# shellcheck disable=2046
+sed --in-place --regexp-extended 's|// chrome-only: ||' $(find "${DIST_DIRECTORY}" -name '*.js' -o -name '*.html')
 
 # create zip
 name="$(jq -r '.name' manifest.json |
